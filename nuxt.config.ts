e7070e32aba7f4c44a9b77541ed2055435429eb6
Alexandr Ojog <ojog.al@gmail.com>
2023-09-16 22:45:29 +0300

    config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,37 @@ process.env.STATIC_REFERRAL_DOCS = files.reverse().join("$")
 
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  modules: [
+    '@zadigetvoltaire/nuxt-gtm',
+    '@nuxtjs/google-analytics',
+  ],
   css: ['~/assets/scss/main.scss'],
-  runtimeConfig: { public: { files: process.env.STATIC_REFERRAL_DOCS || "THERE IS NO FILES MON" } },
+  runtimeConfig: { 
+    public: { 
+      files: process.env.STATIC_REFERRAL_DOCS || "",
+      googleAnalytics: {
+        id: process.env.GOOGLE_ANALYTICS_ID || "G-L2CLJS9S80",
+      },
+      gtm: {
+        id: process.env.GTM_ID || 'GTM-WFBGWDD4',
+        queryParams: {
+          gtm_auth: 'AB7cDEf3GHIjkl-MnOP8qr',
+          gtm_preview: 'env-4',
+          gtm_cookies_win: 'x',
+        },
+        defer: false,
+        compatibility: false,
+        nonce: '2726c7f26c',
+        enabled: true,
+        debug: true,
+        loadScript: true,
+        enableRouterSync: true,
+        ignoredViews: ['homepage'],
+        trackOnNextTick: false,
+        devtools: true,
+      }
+    } 
+  },
   imports: { dirs: [ "composables/**" ] }
 })
